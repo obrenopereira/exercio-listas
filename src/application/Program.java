2f98ce06507f3be3805b7e2614d0c38e8a667ff5
obrenopereira <brenopereiragms@gmail.com>
2022-07-27 22:26:47 -0300

    edit listEmployees

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,9 @@
 package application;
 
+import entities.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,8 +13,32 @@ public class Program {
         Scanner read = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        System.out.println("How many employees will be registered?");
+        System.out.print("How many employees will be registered? ");
         int numberEmployee = read.nextInt();
+
+        Employee employee;
+        List<Employee> listEmployees = new ArrayList<>();
+
+        for (int i = 1; i <= numberEmployee; i++){
+            System.out.println("Employee #" + i + ":");
+            read.nextLine();
+            System.out.print("Id: ");
+            int id = read.nextInt();
+
+            read.nextLine();
+            System.out.print("Name: ");
+            String name = read.nextLine();
+            System.out.print("Salary: ");
+            double salary = read.nextDouble();
+
+            listEmployees.add(new Employee(id, name, salary));
+        }
+        for (int i = 1; i <= numberEmployee; i++){
+
+            System.out.println();
+        }
+
+
 
 
 
