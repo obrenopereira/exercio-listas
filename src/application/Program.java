6e0bb49348863024f143567d781ada4a128b2001
obrenopereira <brenopereiragms@gmail.com>
2022-07-27 21:42:50 -0300

    edit entities package

---
+++ src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("How many employees will be registered?");
+        int numberEmployee = read.nextInt();
+
+
+
+    }
+
+
+}
