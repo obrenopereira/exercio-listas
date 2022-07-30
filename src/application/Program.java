7a406e933c1077d72232648bdd36f225897a3f2b
obrenopereira <brenopereiragms@gmail.com>
2022-07-30 01:54:21 -0300

    final git

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,10 +20,15 @@ public class Program {
         List<Employee> listEmployees = new ArrayList<>();
 
         for (int i = 1; i <= numberEmployee; i++){
-            System.out.println("Employee #" + i + ":");
+            System.out.println("Employee #" + (i +1 ) + ":");
             read.nextLine();
             System.out.print("Id: ");
             int id = read.nextInt();
+            while (hasId(listEmployees, id)){
+                System.out.println("Id already taken! Try again:");
+                id = read.nextInt();
+            }
+
 
             read.nextLine();
             System.out.print("Name: ");
@@ -31,16 +36,40 @@ public class Program {
             System.out.print("Salary: ");
             double salary = read.nextDouble();
 
-            listEmployees.add(new Employee(id, name, salary));
-        }
-        for (int i = 1; i <= numberEmployee; i++){
-
-            System.out.println();
+            Employee emp = new Employee(id, name, salary);
+            listEmployees.add(emp);
         }
 
+        System.out.println();
+        System.out.println("Enter the employee id that will have salary increase : ");
+        int idSalary = read.nextInt();
+        Integer pos = position (listEmployees, idSalary);
 
+        if(pos == null){
+            System.out.println("This id does not exist!");
+        }else{
+            System.out.print("Enter the percentage: ");
+            double percentage = read.nextDouble();
+            listEmployees.get(pos).increaseSalary(percentage);
+        }
+        System.out.println();
+        System.out.println("List of employees: ");
+        for ( Employee emp: listEmployees) {
+            System.out.println(emp);
+        }
+    }
+    static Integer position(List<Employee> listEmployees, int id){
+        for (int i = 0; i < listEmployees.size(); i++){
+            if(listEmployees.get(i).getId() == id){
+                return i;
+            }
+        }
+        return null;
+    }
 
-
+    public static boolean hasId(List<Employee> list, int id){
+        Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
 
     }
 
